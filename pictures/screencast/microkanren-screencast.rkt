640b8c1299091b651e497813197af4b09e36a369
Srikumar K. S <srikumar.subramanian@krea.edu.in>
2022-10-22 08:03:42 +0530

    Unsaved version

--- pictures/screencast/microkanren-screencast.rkt
+++ pictures/screencast/microkanren-screencast.rkt
@@ -57,9 +57,9 @@ BSet - association list
   (cond
     [(and (Var? Av) (Var? Bv) (eq? Av Bv))
      bset]
-    [(Var? Av)
+    [(and (Var? Av) (not (occurs? Av Bv)))
      (extend Av Bv bset)]
-    [(Var? Bv)
+    [(and (Var? Bv) (not (occurs? Bv Av)))
      (extend Bv Av bset)]
     [(and (pair? Av)
           (pair? Bv))
@@ -75,12 +75,78 @@ BSet - association list
      bset]
     [else #f])))
 
+(define (occurs? A P)
+  (cond
+    [(eq? A P) #t]
+    [(and (pair? P) (empty? P)) #f]
+    [(and (pair? P) (not (empty? P)))
+     (or (occurs? A (car P))
+         (occurs? A (cdr P)))]
+    [else #f]))
+       
+
+; (eq A B)
+; A B BSet -> BSet | #f
+; A = 3 or A = 4
+; Goal :: BSet -> Listof BSet
+
+(define (eq A B)
+  (λ (bset)
+    (let ([m (unify A B bset)])
+      (if m
+          (list m)
+          empty))))
+
+; bset1 :: (...)
+; bset2 :: (...)
+; bset3 :: (...)
+;
+; bset
+; goalA -> (bset1 bset2)
+; goalB -> (bset3)
+; -> concat -> (bset1 bset2 bset3)
+; "disjunction"
+(define (disj goalA goalB)
+  (λ (bset)
+    (append (goalA bset) (goalB bset))))
+
+; "and" = "conjunction"
+(define (conj goalA goalB)
+  (λ (bset)
+    (apply append (map goalB (goalA bset)))))
+
+; (disj goalA goalB) = (disj goalB goalA)
+; (conj goalA goalB) = (conj goalB goalA)
+
+; (apply append (map goalB (goalA bset)))
+; =? (apply append (map goalA (goalB bset)))
+
+
+; unify, conj, disj, variables, pairs
+; Prolog
+; A = B
+; Alpha alpha
+; [1,2,3]
+; (cons x xs) = [x | xs]
+; conj => ,
+; disj => ;
+; predicate(Arg1,Arg2,...)    "symbolic expressions"
+; "functor"
+; functor(Arg1, Arg2, ...)
+; (struct AddC (x y))
+; addC(x, y) === (AddC x y)
+; mypredicate(A,B) :-
+;     goalA(A, 23),
+;     goalB(B, 45).
+      
+
+
 (define A (Var 'a))
 (define B (Var 'b))
 
 ; (cons A 3) = (cons 4 B)
 ; A = 4 & 3 = B
-; 
+; A = (cons 1 A)
 
   
        
